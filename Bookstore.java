b648b6125d8abee38ad6233286c6bca9bbc3d119
keeganripley <71792503+keeganripley@users.noreply.github.com>
2020-09-25 21:20:07 -0500

    Update Bookstore.java

--- Bookstore.java
+++ Bookstore.java
@@ -7,63 +7,61 @@
 // Lecturer: Florian
 // Notes to Grader: <optional extra notes>
 public class Bookstore<KeyType, ValueType> {
+	private int isbn; // ISBN number but without first 3 numbers (978); therefore only stores last 10
+	// digits of ISBN # (starts with 1 or 0)
+	private String title; // book title
+	private int price; // book price
+	private String genre; // genre of book
+	private int quantity; // number of books currently available
+
 	// KeyIsbn is last 10 digits of ISBN of book
 	// ValueBook is a book object that has 5 parameters:
 	// isbn, title, price, genre, quantity
 	// To retrieve, get method for desired stored value
 	HashTableMap<Integer, Book> bookstore = new HashTableMap<Integer, Book>(20);
-	Book newBook = new Book(isbn, title, price, genre, quantity);
-	
 
 	// put() method alteration
 	// if key exists, then quantity++
 	// if key doesn't exists, put book into hash table
-	public void put(Integer isbn, Book newBook) {
-		if (bookstore.containsKey(isbn)) {
-			newBook.setQuantity(quantity++);
+	public void put(Integer key, Book newBook) {
+		// set global variables to equal the information of the parameter book
+		isbn = newBook.getIsbn();
+		title = newBook.getTitle();
+		genre = newBook.getGenre();
+		quantity = newBook.getQuantity();
+		// Create new book that has information held in global variables
+		Book book = new Book(isbn, title, price, genre, quantity);
+		// If ISBN exists, add one to the quantity.
+		if (bookstore.containsKey(key)) {
+			book.setQuantity(quantity++);
+		// Otherwise add a new book at the index of the hashed ISBN.
 		} else {
-			Book newBookInfo = new Book(isbn, null, isbn, null, isbn);
-			newBook = newBookInfo;
+		bookstore.put(isbn, book);
 		}
 	}
-	
+
 	// remove() method alteration
 	// quantity--
 	// if quantity == 0, remove book
 	public void remove(Integer isbn) {
-		int index = bookstore.hash(isbn);
-		// Find book at index, decrease quantity by 1
-		if (newBook.getQuantity() == 0) {
+		// If quantity = 0 then remove the book.
+		if (bookstore.get(isbn).getQuantity() == 0) {
 			bookstore.remove(isbn);
+		// Find book at index, decrease quantity by 1
 		} else {
-			newBook.setQuantity(quantity--);
+			bookstore.get(isbn).setQuantity(quantity--);
 		}
-		
 	}
 
-	// int buy() method
-	// if containsKey == true
-	// then return getPrice of book
-	public int buy(Integer isbn) {
-		int price = newBook.getPrice();
-		bookstore.remove(isbn);
-		return price;
-	}
-	
 	// donate(isbn, book)
-	// would be same as put method no?
 	public void donate(Integer isbn, Book newBook) {
 		bookstore.put(isbn, newBook);
 	}
-	
+
 	// steal method
 	// run clear() then return "we've been robbed of knowledge!")
 	public void steal() {
 		bookstore.clear();
 		System.out.println("We've been robbed of knowledge!");
 	}
-	
-
-	// Possible implementations
-	// Create new hash table class--won't work for other members hash table classes
 }
