fcdfd8e63eeb8e49539bf4432b5336bdf3950063
keeganripley <71792503+keeganripley@users.noreply.github.com>
2020-09-25 21:24:51 -0500

    Update Bookstore.java

--- Bookstore.java
+++ Bookstore.java
@@ -1,67 +1,77 @@
-// --== CS400 File Header Information ==--
-// Name: Keegan Ripley
-// Email: kjripley@wisc.edu
-// Team: EE
-// Role: Backend Developer
-// TA: Keren
-// Lecturer: Florian
-// Notes to Grader: <optional extra notes>
-public class Bookstore<KeyType, ValueType> {
-	private int isbn; // ISBN number but without first 3 numbers (978); therefore only stores last 10
-	// digits of ISBN # (starts with 1 or 0)
-	private String title; // book title
-	private int price; // book price
-	private String genre; // genre of book
-	private int quantity; // number of books currently available
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-	// KeyIsbn is last 10 digits of ISBN of book
-	// ValueBook is a book object that has 5 parameters:
-	// isbn, title, price, genre, quantity
-	// To retrieve, get method for desired stored value
-	HashTableMap<Integer, Book> bookstore = new HashTableMap<Integer, Book>(20);
+/**
+ * This represents a collection of books stored in an array
+ * 
+ * @author elaine
+ *
+ */
+public class BookCollection {
+	private Book[] initialBooksArray; // book array to store book
+	private int size; // number of different books stored in collection
 
-	// put() method alteration
-	// if key exists, then quantity++
-	// if key doesn't exists, put book into hash table
-	public void put(Integer key, Book newBook) {
-		// set global variables to equal the information of the parameter book
-		isbn = newBook.getIsbn();
-		title = newBook.getTitle();
-		genre = newBook.getGenre();
-		quantity = newBook.getQuantity();
-		// Create new book that has information held in global variables
-		Book book = new Book(isbn, title, price, genre, quantity);
-		// If ISBN exists, add one to the quantity.
-		if (bookstore.containsKey(key)) {
-			book.setQuantity(quantity++);
-		// Otherwise add a new book at the index of the hashed ISBN.
-		} else {
-		bookstore.put(isbn, book);
+	/**
+	 * Gets the array of books
+	 * 
+	 * @return the initialBooksArray
+	 */
+	public Book[] getInitialBooks() {
+		return initialBooksArray;
+	}
+
+	/**
+	 * Creates instance of book collection
+	 * 
+	 * @param initialBooksArray
+	 */
+	public BookCollection(int capacity) {
+		super();
+		this.initialBooksArray = new Book[capacity];
+	}
+
+	/**
+	 * Adds initial list of book data into book collection
+	 * 
+	 * @throws FileNotFoundException if initial list of books is not found
+	 * 
+	 */
+	public void addInitialBooksToTable() throws FileNotFoundException {
+		File file = new File("BookData.txt");
+		Scanner inFile = new Scanner(file);
+
+		int isbn = 0;
+		String title = null; // spaces are indicated with _
+		String genre = null;
+		int quantity = 0;
+
+		while (inFile.hasNextLine()) {
+			String line = inFile.nextLine();
+			Scanner readLine = new Scanner(line);
+
+			while (readLine.hasNext()) {
+				isbn = readLine.nextInt(); // first 3 numbers (978) of actual ISBN # are cut out, thus isbn
+											// variable stores last 10 digits of actual ISBN # (starts with 1
+											// or 0)
+				title = readLine.next();
+				genre = readLine.next();
+				quantity = readLine.nextInt();
+			}
+			// creates book instances and adds to book collection array
+			Book book = new Book(isbn, title, genre, quantity);
+			initialBooksArray[size] = book;
+			size++;
 		}
 	}
 
-	// remove() method alteration
-	// quantity--
-	// if quantity == 0, remove book
-	public void remove(Integer isbn) {
-		// If quantity = 0 then remove the book.
-		if (bookstore.get(isbn).getQuantity() == 0) {
-			bookstore.remove(isbn);
-		// Find book at index, decrease quantity by 1
-		} else {
-			bookstore.get(isbn).setQuantity(quantity--);
-		}
+	/**
+	 * Gets size of array
+	 * 
+	 * @return the size number of different books stored
+	 */
+	public int getSize() {
+		return size;
 	}
 
-	// donate(isbn, book)
-	public void donate(Integer isbn, Book newBook) {
-		bookstore.put(isbn, newBook);
-	}
-
-	// steal method
-	// run clear() then return "we've been robbed of knowledge!")
-	public void steal() {
-		bookstore.clear();
-		System.out.println("We've been robbed of knowledge!");
-	}
 }
