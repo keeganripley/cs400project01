7bf91784961725ab273ecb33e7ab1ad689c4170c
keeganripley <71792503+keeganripley@users.noreply.github.com>
2020-09-25 21:23:01 -0500

    Update BookCollection.java

--- BookCollection.java
+++ BookCollection.java
@@ -9,75 +9,69 @@ import java.util.Scanner;
  *
  */
 public class BookCollection {
-  private Book[] initialBooksArray; // book array to store book
-  private int size; // number of different books stored in collection
+	private Book[] initialBooksArray; // book array to store book
+	private int size; // number of different books stored in collection
 
+	/**
+	 * Gets the array of books
+	 * 
+	 * @return the initialBooksArray
+	 */
+	public Book[] getInitialBooks() {
+		return initialBooksArray;
+	}
 
-  /**
-   * Gets the array of books
-   * 
-   * @return the initialBooksArray
-   */
-  public Book[] getInitialBooks() {
-    return initialBooksArray;
-  }
+	/**
+	 * Creates instance of book collection
+	 * 
+	 * @param initialBooksArray
+	 */
+	public BookCollection(int capacity) {
+		super();
+		this.initialBooksArray = new Book[capacity];
+	}
 
+	/**
+	 * Adds initial list of book data into book collection
+	 * 
+	 * @throws FileNotFoundException if initial list of books is not found
+	 * 
+	 */
+	public void addInitialBooksToTable() throws FileNotFoundException {
+		File file = new File("BookData.txt");
+		Scanner inFile = new Scanner(file);
 
-  /**
-   * Creates instance of book collection
-   * 
-   * @param initialBooksArray
-   */
-  public BookCollection(int capacity) {
-    super();
-    this.initialBooksArray = new Book[capacity];
-  }
+		int isbn = 0;
+		String title = null; // spaces are indicated with _
+		String genre = null;
+		int quantity = 0;
 
+		while (inFile.hasNextLine()) {
+			String line = inFile.nextLine();
+			Scanner readLine = new Scanner(line);
 
-  /**
-   * Adds initial list of book data into book collection
-   * 
-   * @throws FileNotFoundException if initial list of books is not found
-   * 
-   */
-  public void addInitialBooksToTable() throws FileNotFoundException {
-    File file = new File("BookData.txt");
-    Scanner inFile = new Scanner(file);
+			while (readLine.hasNext()) {
+				isbn = readLine.nextInt(); // first 3 numbers (978) of actual ISBN # are cut out, thus isbn
+											// variable stores last 10 digits of actual ISBN # (starts with 1
+											// or 0)
+				title = readLine.next();
+				genre = readLine.next();
+				quantity = readLine.nextInt();
+			}
+			// creates book instances and adds to book collection array
+			Book book = new Book(isbn, title, genre, quantity);
+			initialBooksArray[size] = book;
+			size++;
+		}
+	}
 
-    int isbn = 0;
-    String title = null; // spaces are indicated with _
-    int price = 0;
-    String genre = null;
-    int quantity = 0;
-
-    while (inFile.hasNextLine()) {
-      String line = inFile.nextLine();
-      Scanner readLine = new Scanner(line);
-
-      while (readLine.hasNext()) {
-        isbn = readLine.nextInt(); // first 3 numbers (978) of actual ISBN # are cut out, thus isbn
-                                   // variable stores last 10 digits of actual ISBN # (starts with 1
-                                   // or 0)
-        title = readLine.next();
-        price = readLine.nextInt();
-        genre = readLine.next();
-        quantity = readLine.nextInt();
-      }
-      // creates book instances and adds to book collection array
-      Book book = new Book(isbn, title, price, genre, quantity);
-      initialBooksArray[size] = book;
-      size++;
-    }
-  }
-
-
-  /**
-   * Gets size of array
-   * 
-   * @return the size number of different books stored
-   */
-  public int getSize() {
-    return size;
-  }
+	/**
+	 * Gets size of array
+	 * 
+	 * @return the size number of different books stored
+	 */
+	public int getSize() {
+		return size;
+	}
 
 }
